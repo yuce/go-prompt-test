7a04e84719499a8de56f45e1d37a208666e93432
Yuce Tekol <yucetekol@gmail.com>
2021-03-30 08:40:46 +0300

    Gowid test

--- cmd/hzsqlcl/main.go
+++ cmd/hzsqlcl/main.go
@@ -1,9 +1,167 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"fmt"
+	ui "github.com/VladimirMarkelov/clui"
+	"github.com/gcla/gowid"
+	"github.com/gcla/gowid/widgets/divider"
+	"github.com/gcla/gowid/widgets/pile"
+	"github.com/gcla/gowid/widgets/styled"
+	"github.com/gcla/gowid/widgets/text"
+	"github.com/gcla/gowid/widgets/vpadding"
+	"github.com/gdamore/tcell"
+	"log"
+	"time"
 )
 
+var txt *text.Widget
+
+func unhandled(app gowid.IApp, ev interface{}) bool {
+	if evk, ok := ev.(*tcell.EventKey); ok {
+		switch evk.Rune() {
+		case 'q', 'Q':
+			app.Quit()
+		default:
+			txt.SetText(fmt.Sprintf("hello world - %c", evk.Rune()), app)
+		}
+	}
+	return true
+}
+
+func createMessage(msg string, color string) *text.Content {
+	txtSegment := text.StyledContent(msg, gowid.MakePaletteRef(color))
+	return text.NewContent([]text.ContentSegment{
+		txtSegment,
+	})
+}
+
+func createHintMessage(msg string) *text.Content {
+	return createMessage(msg, "banner")
+}
+
+func createErrorMessage(msg string) *text.Content {
+	return createMessage(msg, "error")
+}
+
+func main() {
+	palette := gowid.Palette{
+		"banner": gowid.MakePaletteEntry(gowid.ColorBlack, gowid.NewUrwidColor("light gray")),
+		"streak": gowid.MakePaletteEntry(gowid.ColorBlack, gowid.ColorRed),
+		"bg":     gowid.MakePaletteEntry(gowid.ColorBlack, gowid.ColorDarkBlue),
+		"error":  gowid.MakePaletteEntry(gowid.ColorWhite, gowid.ColorRed),
+	}
+	txtSegment := text.StyledContent("Hit tab to auto-complete", gowid.MakePaletteRef("banner"))
+	txt := text.NewFromContentExt(
+		text.NewContent([]text.ContentSegment{
+			txtSegment,
+		}), text.Options{
+			Align: gowid.HAlignLeft{},
+		},
+	)
+	vert := vpadding.New(txt, gowid.VAlignBottom{}, gowid.RenderFlow{})
+	app, _ := gowid.NewApp(gowid.AppArgs{
+		View:    vert,
+		Palette: palette,
+	})
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		hintMsg := createHintMessage("create mapping MAPPING_NAME MAPPINT TYPE")
+		txt.SetContent(app, hintMsg)
+		app.Redraw()
+		time.Sleep(2 * time.Second)
+		errorMsg := createErrorMessage("ERROR: connection to the server was lost")
+		txt.SetContent(app, errorMsg)
+		app.Redraw()
+	}()
+
+	app.SimpleMainLoop()
+}
+
+func main4() {
+	txt = text.New("hello")
+	if app, err := gowid.NewApp(gowid.AppArgs{View: txt}); err != nil {
+		log.Fatal(err)
+	} else {
+		app.MainLoop(gowid.UnhandledInputFunc(unhandled))
+	}
+}
+
+func main3() {
+	txt := text.New("hello, world!")
+	if app, err := gowid.NewApp(gowid.AppArgs{View: txt}); err != nil {
+		log.Fatal(err)
+	} else {
+		app.SimpleMainLoop()
+	}
+
+}
+
+func main2() {
+	palette := gowid.Palette{
+		"banner":  gowid.MakePaletteEntry(gowid.ColorWhite, gowid.MakeRGBColor("#60d")),
+		"streak":  gowid.MakePaletteEntry(gowid.ColorNone, gowid.MakeRGBColor("#60a")),
+		"inside":  gowid.MakePaletteEntry(gowid.ColorNone, gowid.MakeRGBColor("#808")),
+		"outside": gowid.MakePaletteEntry(gowid.ColorNone, gowid.MakeRGBColor("#a06")),
+		"bg":      gowid.MakePaletteEntry(gowid.ColorNone, gowid.MakeRGBColor("#d06")),
+	}
+
+	div := divider.NewBlank()
+	outside := styled.New(div, gowid.MakePaletteRef("outside"))
+	inside := styled.New(div, gowid.MakePaletteRef("inside"))
+
+	helloworld := styled.New(
+		text.NewFromContentExt(
+			text.NewContent([]text.ContentSegment{
+				text.StyledContent("Now, for something completely different.", gowid.MakePaletteRef("banner")),
+			}),
+			text.Options{
+				Align: gowid.HAlignMiddle{},
+			},
+		),
+		gowid.MakePaletteRef("streak"),
+	)
+
+	f := gowid.RenderFlow{}
+
+	view := styled.New(
+		vpadding.New(
+			pile.New([]gowid.IContainerWidget{
+				&gowid.ContainerWidget{IWidget: outside, D: f},
+				&gowid.ContainerWidget{IWidget: inside, D: f},
+				&gowid.ContainerWidget{IWidget: helloworld, D: f},
+				&gowid.ContainerWidget{IWidget: inside, D: f},
+				&gowid.ContainerWidget{IWidget: outside, D: f},
+			}),
+			gowid.VAlignMiddle{},
+			f),
+		gowid.MakePaletteRef("bg"),
+	)
+
+	app, _ := gowid.NewApp(gowid.AppArgs{
+		View:    view,
+		Palette: &palette,
+	})
+
+	app.SimpleMainLoop()
+}
+
+func main1() {
+	ui.InitLibrary()
+	defer ui.DeinitLibrary()
+
+	view1 := ui.AddWindow(0, 0, 10, 7, "Hello, World!")
+	btnQuit := ui.CreateButton(view1, 15, 4, "Hi", 1)
+	btnQuit.OnClick(func(event ui.Event) {
+		go ui.Stop()
+	})
+
+	ui.AddWindow(10, 5, 10, 10, "Foo")
+
+	ui.MainLoop()
+}
+
+/*
 func main() {
 	app := tview.NewApplication()
 	form := tview.NewForm().
@@ -25,3 +183,4 @@ func main() {
 		panic(err)
 	}
 }
+*/
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module hzsqlcl
 go 1.15
 
 require (
-	github.com/c-bata/go-prompt v0.2.6
+	github.com/VladimirMarkelov/clui v1.2.1 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/c-bata/go-prompt v0.2.6 // indirect
 	github.com/candid82/liner v1.4.0 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/gcla/gowid v1.2.0 // indirect
 	github.com/hazelcast/hazelcast-go-client/v4 v4.0.0
-	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/nsf/termbox-go v1.1.0 // indirect
+	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598
 )
 
-replace github.com/hazelcast/hazelcast-go-client/v4 v4.0.0 => github.com/yuce/hazelcast-go-client/v4 v4.0.0-temp.2
+replace github.com/hazelcast/hazelcast-go-client/v4 v4.0.0 => github.com/yuce/hazelcast-go-client/v4 v4.0.0-temp.3
