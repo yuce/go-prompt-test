fa4f3bf5dd7da4804e5b15cf6755ac1539086413
Yuce Tekol <yucetekol@gmail.com>
2021-03-31 03:53:47 +0300

    simplified LabeledEdit

--- form/edit.go
+++ form/edit.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gcla/gowid"
-	"github.com/gcla/gowid/widgets/columns"
 	"github.com/gcla/gowid/widgets/edit"
-	"github.com/gcla/gowid/widgets/text"
 )
 
 type LabeledEdit struct {
@@ -15,17 +13,14 @@ type LabeledEdit struct {
 }
 
 func NewLabeledEdit(target *string, label string) *LabeledEdit {
-	labelWidget := text.New(label)
 	widget := &LabeledEdit{target: target}
-	editWidget := edit.New()
+	editWidget := edit.New(edit.Options{
+		Caption: label,
+	})
 	editWidget.OnTextSet(gowid.WidgetCallback{fmt.Sprintf("edit%s", label), func(app gowid.IApp, w gowid.IWidget) {
 		edt := w.(*edit.Widget)
 		*widget.target = edt.Text()
 	}})
-	cols := []gowid.IContainerWidget{
-		&gowid.ContainerWidget{labelWidget, gowid.RenderWithWeight{W: 1}},
-		&gowid.ContainerWidget{editWidget, gowid.RenderWithWeight{W: 1}},
-	}
-	widget.IWidget = columns.New(cols)
+	widget.IWidget = editWidget
 	return widget
 }
